5b687c2c67999f163b9a2f538200f6655fe526aa
odiez3 <odtorres1991@hotmail.com>
2017-08-11 17:45:10 -0500

    Se agrega todo lo correspondiente al player final del curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing, appRoutingProviders } from './app.routing';
+//General
 import { AppComponent } from './app.component';
-import { UserEditComponent } from './components/user-edit.component';
-import { ArtistListComponent } from './components/artist-list.component';
 import { HomeComponent } from './components/home.component';
+//User
+import { UserEditComponent } from './components/user-edit.component';
+
+//Artist
+import { ArtistListComponent } from './components/artist-list.component';
 import { ArtistAddComponent } from './components/artist-add.component';
 import { ArtistEditComponent } from './components/artist-edit.component';
 import { ArtistDetailComponent } from './components/artist-detail.component';
+//Albums
 import { AlbumAddComponent } from './components/album-add.component';
 import { AlbumEditComponent } from './components/album-edit.component';
 import { AlbumDetailComponent } from './components/album-detail.component';
+//Song
 import { SongAddComponent } from './components/song-add.component';
 import { SongEditComponent } from './components/song-edit.component';
+import { PlayerComponent } from './components/player.component';
 
 
 @NgModule({
@@ -30,6 +37,7 @@ import { SongEditComponent } from './components/song-edit.component';
     AlbumDetailComponent,
     SongAddComponent,
     SongEditComponent,
+    PlayerComponent,
     HomeComponent
   ],
   imports: [
